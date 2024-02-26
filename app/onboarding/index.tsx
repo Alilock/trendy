1abe75fc1e22d8eb275b136dc2721091710f5a24
Ali <105427281+Alilock@users.noreply.github.com>
2024-02-26 22:51:35 +0400

    Add scheme and plugins to app.json, update main in package.json

---
+++ app/onboarding/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const Onboarding = () => {
+    return (
+        <View>
+            <Text>salam</Text>
+        </View>
+    );
+}
+export default Onboarding;
